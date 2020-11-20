821576e59e299d049f2bc542a49922986a795b27
KimberKoding <46941868+KimberKoding@users.noreply.github.com>
2020-11-20 13:34:41 +0000

    Updated Design of Login Page & Added Registration Page

--- app/src/main/java/com/example/tencil/MainActivity.java
+++ app/src/main/java/com/example/tencil/MainActivity.java
@@ -1,29 +1,53 @@
 package com.example.tencil;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
-    private TextView greetingLabel;
-    private EditText textInputField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        greetingLabel = findViewById(R.id.greeting);
-        textInputField = findViewById(R.id.textInput);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new LoginFragment());
+        pagerAdapter.addFragment(new RegisterFragment());
+        viewPager.setAdapter(pagerAdapter);
     }
 
-    public void changeGreeting(View view) {
-        String greeting = textInputField.getText().toString();
-        //Set the text
-        greetingLabel.setText(greeting);
+    class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+        private final ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+        public AuthenticationPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        void addFragment(Fragment fragment) {
+            fragmentList.add(fragment);
+        }
     }
 }
