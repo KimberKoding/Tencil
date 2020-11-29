f0dea687c972accc3b157f73f2e4168fd5a5d5b1
KimberKoding <46941868+KimberKoding@users.noreply.github.com>
2020-11-29 13:48:03 +0000

    Added menu navigation

--- app/src/main/java/com/example/tencil/dashboard.java
+++ app/src/main/java/com/example/tencil/dashboard.java
@@ -1,30 +1,77 @@
 package com.example.tencil;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 
-abstract class openDash extends AppCompatActivity implements View.OnClickListener {
+public class dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Variables
+    ImageView menuIcon;
+    LinearLayout contentView;
+
+    //Drawer Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
+        getWindow ().setFlags ( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView ( R.layout.activity_dashboard );
-        Button button = (Button) findViewById ( R.id.button );
-        button.setOnClickListener ( v -> openIndustry () );
 
+        //Hooks
+        menuIcon = findViewById ( R.id.menu_icon );
+        contentView = findViewById ( R.id.content );
+
+        //Menu Hooks
+        drawerLayout = findViewById ( R.id.drawer_layout );
+        navigationView = findViewById ( R.id.navigation_view );
+
+        navigationDrawer ();
 
     }
 
-    public void openIndustry() {
-        Intent intent = new Intent ( this, industry.class );
-        startActivity ( intent );
+    //Navigation Drawer Functions
+    private void navigationDrawer() {
+
+        //Naviagtion Drawer
+        navigationView.bringToFront ();
+        navigationView.setNavigationItemSelectedListener ( this );
+        navigationView.setCheckedItem ( R.id.nav_home );
+
+        menuIcon.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible ( GravityCompat.START ))
+                    drawerLayout.closeDrawer ( GravityCompat.START );
+                else drawerLayout.openDrawer ( GravityCompat.START );
+            }
+        } );
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerVisible ( GravityCompat.START )) {
+            drawerLayout.closeDrawer ( GravityCompat.START );
+        } else
+            super.onBackPressed ();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
 
 }
-
